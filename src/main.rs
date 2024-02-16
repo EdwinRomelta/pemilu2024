3744c6d4f834544939eb15b759dc973f6f19cbc0
Edwin Romelta <romelta.edwin@gmail.com>
2024-02-16 19:46:54 +0700

    feat: Fetch vote count data

--- src/main.rs
+++ src/main.rs
@@ -12,48 +12,105 @@ struct Region {
     tingkat: i8,
 }
 
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+struct Vote {
+    table: HashMap<String, Count>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+struct Count {
+    #[serde(rename = "100025")]
+    i01: Option<i32>,
+    #[serde(rename = "100026")]
+    i02: Option<i32>,
+    #[serde(rename = "100027")]
+    i03: Option<i32>,
+    psu: String,
+    persen: Option<f32>,
+    status_progress: Option<bool>,
+}
+
+#[derive(Debug, Clone)]
+struct VoteResult {
+    nama: String,
+    id: i32,
+    kode: String,
+    tingkat: i8,
+    i01: i32,
+    i02: i32,
+    i03: i32,
+}
+
 async fn populate_vote() -> Result<(), Error> {
     let region1 = populate_region_1().await;
+    let vote1 = vote_count_region_1().await;
+    let vote_results1 = join_vote(region1.clone(), vote1);
 
-    let region2_tasks: Vec<_> = region1
+    let region2_tasks: Vec<_> = vote_results1
         .into_iter()
-        .map(|region| tokio::spawn(async { populate_region_2(region).await }))
+        .map(|vote_results| {
+            tokio::spawn(async {
+                let region = populate_region_2(vote_results.clone()).await;
+                let vote = vote_count_region_2(vote_results).await;
+                join_vote(region, vote)
+            })
+        })
         .collect();
-    let region2 = futures::future::join_all(region2_tasks)
+    let vote_results2 = futures::future::join_all(region2_tasks)
         .await
         .into_iter()
         .flat_map(|regions| regions.unwrap())
-        .collect::<Vec<Region>>();
+        .collect::<Vec<VoteResult>>();
 
-    let region3_tasks: Vec<_> = region2
+    let region3_tasks: Vec<_> = vote_results2
         .into_iter()
-        .map(|region| tokio::spawn(async { populate_region_3(region).await }))
+        .map(|vote_results| {
+            tokio::spawn(async {
+                let region = populate_region_3(vote_results.clone()).await;
+                let vote = vote_count_region_3(vote_results).await;
+                join_vote(region, vote)
+            })
+        })
         .collect();
-    let region3 = futures::future::join_all(region3_tasks)
+    let vote_results3 = futures::future::join_all(region3_tasks)
         .await
         .into_iter()
         .flat_map(|regions| regions.unwrap())
-        .collect::<Vec<Region>>();
+        .collect::<Vec<VoteResult>>();
 
-    let region4_tasks: Vec<_> = region3
+    let region4_tasks: Vec<_> = vote_results3
         .into_iter()
-        .map(|region| tokio::spawn(async { populate_region_4(region).await }))
+        .map(|vote_results| {
+            tokio::spawn(async {
+                let region = populate_region_4(vote_results.clone()).await;
+                let vote = vote_count_region_4(vote_results).await;
+                join_vote(region, vote)
+            })
+        })
         .collect();
-    let region4 = futures::future::join_all(region4_tasks)
+    let vote_results4 = futures::future::join_all(region4_tasks)
         .await
         .into_iter()
         .flat_map(|regions| regions.unwrap())
-        .collect::<Vec<Region>>();
+        .collect::<Vec<VoteResult>>();
 
-    let region5_tasks: Vec<_> = region4
+    let region5_tasks: Vec<_> = vote_results4
         .into_iter()
-        .map(|region| tokio::spawn(async { populate_region_5(region).await }))
+        .map(|vote_results| {
+            tokio::spawn(async {
+                let region = populate_region_5(vote_results.clone()).await;
+                let vote = vote_count_region_5(vote_results).await;
+                join_vote(region, vote)
+            })
+        })
         .collect();
-    let region5 = futures::future::join_all(region5_tasks)
+    let vote_results5 = futures::future::join_all(region5_tasks)
         .await
         .into_iter()
         .flat_map(|regions| regions.unwrap())
-        .collect::<Vec<Region>>();
+        .collect::<Vec<VoteResult>>();
     Ok(())
 }
 
@@ -71,10 +128,10 @@ async fn populate_region_1() -> Vec<Region> {
     regions
 }
 
-async fn populate_region_2(region1: Region) -> Vec<Region> {
+async fn populate_region_2(vote_result1: VoteResult) -> Vec<Region> {
     let url = format!(
         "https://sirekap-obj-data.kpu.go.id/wilayah/pemilu/ppwp/{}.json",
-        region1.kode
+        vote_result1.kode
     );
     let client = reqwest::Client::new();
     let response = client
@@ -88,11 +145,11 @@ async fn populate_region_2(region1: Region) -> Vec<Region> {
     regions2
 }
 
-async fn populate_region_3(region2: Region) -> Vec<Region> {
-    let region2_code = &region2.kode[0..2];
+async fn populate_region_3(vote_result2: VoteResult) -> Vec<Region> {
+    let region2_code = &vote_result2.kode[0..2];
     let url = format!(
         "https://sirekap-obj-data.kpu.go.id/wilayah/pemilu/ppwp/{}/{}.json",
-        region2_code, region2.kode
+        region2_code, vote_result2.kode
     );
     let client = reqwest::Client::new();
     let response = client
@@ -106,12 +163,12 @@ async fn populate_region_3(region2: Region) -> Vec<Region> {
     regions2
 }
 
-async fn populate_region_4(region3: Region) -> Vec<Region> {
-    let region2_code = &region3.kode[0..2];
-    let region3_code = &region3.kode[0..4];
+async fn populate_region_4(vote_result3: VoteResult) -> Vec<Region> {
+    let region2_code = &vote_result3.kode[0..2];
+    let region3_code = &vote_result3.kode[0..4];
     let url = format!(
         "https://sirekap-obj-data.kpu.go.id/wilayah/pemilu/ppwp/{}/{}/{}.json",
-        region2_code, region3_code, region3.kode
+        region2_code, region3_code, vote_result3.kode
     );
     let client = reqwest::Client::new();
     let response = client
@@ -125,13 +182,13 @@ async fn populate_region_4(region3: Region) -> Vec<Region> {
     regions3
 }
 
-async fn populate_region_5(region4: Region) -> Vec<Region> {
-    let region2_code = &region4.kode[0..2];
-    let region3_code = &region4.kode[0..4];
-    let region4_code = &region4.kode[0..6];
+async fn populate_region_5(vote_result4: VoteResult) -> Vec<Region> {
+    let region2_code = &vote_result4.kode[0..2];
+    let region3_code = &vote_result4.kode[0..4];
+    let region4_code = &vote_result4.kode[0..6];
     let url = format!(
         "https://sirekap-obj-data.kpu.go.id/wilayah/pemilu/ppwp/{}/{}/{}/{}..json",
-        region2_code, region3_code, region4_code, region4.kode
+        region2_code, region3_code, region4_code, vote_result4.kode
     );
     let client = reqwest::Client::new();
     let response = client
@@ -143,6 +200,123 @@ async fn populate_region_5(region4: Region) -> Vec<Region> {
     println!("Status Code {} : {}", url, response.status());
     let regions5 = response.json().await.unwrap();
     regions5
+}
+
+async fn vote_count_region_1() -> Vote {
+    let url = "https://sirekap-obj-data.kpu.go.id/pemilu/hhcw/ppwp.json";
+    let client = reqwest::Client::new();
+    let response = client
+        .get(url.clone())
+        .header("Content-Type", "application/json")
+        .send()
+        .await
+        .unwrap();
+    println!("Status Code {} : {}", url, response.status());
+    let vote: Vote = response.json().await.unwrap();
+    vote
+}
+
+async fn vote_count_region_2(vote_result1: VoteResult) -> Vote {
+    let url = format!(
+        "https://sirekap-obj-data.kpu.go.id/pemilu/hhcw/ppwp/{}.json",
+        vote_result1.kode
+    );
+    let client = reqwest::Client::new();
+    let response = client
+        .get(url.clone())
+        .header("Content-Type", "application/json")
+        .send()
+        .await
+        .unwrap();
+    println!("Status Code {} : {}", url, response.status());
+    let vote: Vote = response.json().await.unwrap();
+    vote
+}
+
+async fn vote_count_region_3(vote_result2: VoteResult) -> Vote {
+    let region2_code = &vote_result2.kode[0..2];
+    let url = format!(
+        "https://sirekap-obj-data.kpu.go.id/pemilu/hhcw/ppwp/{}/{}.json",
+        region2_code, vote_result2.kode
+    );
+    let client = reqwest::Client::new();
+    let response = client
+        .get(url.clone())
+        .header("Content-Type", "application/json")
+        .send()
+        .await
+        .unwrap();
+    println!("Status Code {} : {}", url, response.status());
+    let vote: Vote = response.json().await.unwrap();
+    vote
+}
+
+async fn vote_count_region_4(vote_result2: VoteResult) -> Vote {
+    let region2_code = &vote_result2.kode[0..2];
+    let region3_code = &vote_result2.kode[0..4];
+    let url = format!(
+        "https://sirekap-obj-data.kpu.go.id/pemilu/hhcw/ppwp/{}/{}/{}.json",
+        region2_code, region3_code, vote_result2.kode
+    );
+    let client = reqwest::Client::new();
+    let response = client
+        .get(url.clone())
+        .header("Content-Type", "application/json")
+        .send()
+        .await
+        .unwrap();
+    println!("Status Code {} : {}", url, response.status());
+    let vote: Vote = response.json().await.unwrap();
+    vote
+}
+
+async fn vote_count_region_5(vote_result4: VoteResult) -> Vote {
+    let region2_code = &vote_result4.kode[0..2];
+    let region3_code = &vote_result4.kode[0..4];
+    let region4_code = &vote_result4.kode[0..6];
+    let url = format!(
+        "https://sirekap-obj-data.kpu.go.id/pemilu/hhcw/ppwp/{}/{}/{}/{}.json",
+        region2_code, region3_code, region4_code, vote_result4.kode
+    );
+    let client = reqwest::Client::new();
+    let response = client
+        .get(url.clone())
+        .header("Content-Type", "application/json")
+        .send()
+        .await
+        .unwrap();
+    println!("Status Code {} : {}", url, response.status());
+    let vote: Vote = response.json().await.unwrap();
+    vote
+}
+
+fn join_vote(region: Vec<Region>, vote: Vote) -> Vec<VoteResult> {
+    region
+        .into_iter()
+        .map(|region| {
+            let vote = vote.table.get(&region.kode);
+            match vote {
+                Some(vote) => VoteResult {
+                    nama: region.nama,
+                    id: region.id,
+                    kode: region.kode,
+                    tingkat: region.tingkat,
+                    i01: vote.i01.unwrap_or_else(|| 0),
+                    i02: vote.i02.unwrap_or_else(|| 0),
+                    i03: vote.i03.unwrap_or_else(|| 0),
+                },
+                None => VoteResult {
+                    nama: region.nama,
+                    id: region.id,
+                    kode: region.kode,
+                    tingkat: region.tingkat,
+                    i01: 0,
+                    i02: 0,
+                    i03: 0,
+                },
+            }
+        })
+        .collect::<Vec<VoteResult>>()
 }
 
 #[tokio::main]
